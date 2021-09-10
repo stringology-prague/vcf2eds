ff9061c5cb5557598d2c350164519a776cb55818
Luboš Krčál <lubos.krcal@gmail.com>
2021-09-10 14:23:20 +1200

    Add protein frequency file to supplement random choice

--- scripts/random_eds.py
+++ scripts/random_eds.py
@@ -1,4 +1,5 @@
 from optparse import OptionParser
+import csv
 import random
 import math
 
@@ -21,7 +22,7 @@ class AverageSettings:
     
     def get_number(self):
         number = int(math.fabs(random.gauss(self.number, self.number_dev)))
-        print('number - {}'.format(number))
+        # print('number - {}'.format(number))
         return number
     
     def get_length(self, max_length=None):
@@ -32,14 +33,14 @@ class AverageSettings:
         return rnd_length
 
 
-def get_random_string(alphabet, length):
-    return ''.join([random.choice(alphabet) for _ in range(length)])
+def get_random_string(alphabet, weights, length):
+    return ''.join(random.choices(alphabet, weights=weights, k=length))
 
 
-def get_degenerate_segment(options, alphabet, curr_depth, avg_settings, max_length=None):
-    print('----- RECURSIVE CALL ------')
+def get_degenerate_segment(options, alphabet, weights, curr_depth, avg_settings, max_length=None):
+    # print('----- RECURSIVE CALL ------')
     if curr_depth == options.max_reds_depth:
-        return [get_random_string(alphabet, avg_settings.get_length(max_length))]
+        return [get_random_string(alphabet, weights, avg_settings.get_length(max_length))]
 
     degenerate_segment = []
 
@@ -51,7 +52,7 @@ def get_degenerate_segment(options, alphabet, curr_depth, avg_settings, max_leng
         i = 0
         while i < segment_length:
             if random.random() < options.reds_prob:
-                data = get_degenerate_segment(options, alphabet, curr_depth + 1, avg_settings.create_reduced_copy(), segment_length)
+                data = get_degenerate_segment(options, alphabet, weights, curr_depth + 1, avg_settings.create_reduced_copy(), segment_length)
 
                 if len(data) == 1:
                     segment_str += data[0]
@@ -60,15 +61,15 @@ def get_degenerate_segment(options, alphabet, curr_depth, avg_settings, max_leng
 
                 i += len(data[0])
             else:
-                segment_str += random.choice(alphabet)
+                segment_str += random.choices(alphabet, weights=weights, k=1)[0]
                 i += 1
 
         degenerate_segment.append(segment_str)
 
     if len(degenerate_segment) == 0:
-        degenerate_segment.append(get_random_string(alphabet, avg_settings.get_length(max_length)))
+        degenerate_segment.append(get_random_string(alphabet, weights, avg_settings.get_length(max_length)))
     
-    print('----- END ------')
+    # print('----- END ------')
     return degenerate_segment
 
 
@@ -76,6 +77,8 @@ if __name__ == "__main__":
     parser = OptionParser()
     parser.add_option('-a', '--alphabet', dest='alphabet',
                     help="Alphabet type (valid values: D for DNA, P for Protein")
+    parser.add_option('-f', '--freq-file', dest='freqfile',
+                    help="Frequency file of the alphabet symbols (see protein.freq for example)")
     parser.add_option('-p', '--probability', dest="probability", default=0.15, metavar='NUMBER',
                     type='float', help="Probability of degenerate segment on each position")
     parser.add_option('-e', '--average', dest="average", default=15, metavar='NUMBER',
@@ -102,11 +105,21 @@ if __name__ == "__main__":
     alphabet = DNA_ALPHABET
     if options.alphabet == 'P':
         alphabet = PROTEIN_ALPHABET
-    
+
+    weights = None
+    if options.freqfile:
+        with open(options.freqfile) as f:
+            reader = csv.reader(f, delimiter=" ", skipinitialspace=True)
+            freqs = [r for r in reader]
+            alphabet = [f[1] for f in freqs]
+            sum_weights = sum([int(f[0]) for f in freqs])
+            weights = [float(f[0]) / sum_weights for f in freqs]
+
     if options.probability < 0 or options.probability > 1:
         raise Exception('Probability can be only from interval [0, 1] including 0 and 1.')
 
     print('  alphabet: {} [{}]'.format(options.alphabet, alphabet))
+    print('  weights: {} {}'.format(options.freqfile, weights))
     print('  Degenerate segment pp: {}'.format(options.probability))
     print('  Gauss avg. length: {}'.format(options.average))
     print('  Gauss standard deviation length: {}'.format(options.deviation))
@@ -123,7 +136,7 @@ if __name__ == "__main__":
         if random.random() < options.probability:
             # degenerate segment
             avg_settings = AverageSettings(options.average, options.deviation, options.number, options.number_deviation)
-            degenerate_segment = get_degenerate_segment(options, alphabet, 0, avg_settings)
+            degenerate_segment = get_degenerate_segment(options, alphabet, weights, 0, avg_settings)
 
             if len(degenerate_segment) == 1:
                 eds_str += degenerate_segment[0]
@@ -132,7 +145,7 @@ if __name__ == "__main__":
 
             i += len(degenerate_segment[0])
         else:
-            eds_str += random.choice(alphabet)
+            eds_str += random.choices(alphabet, weights=weights, k=1)[0]
             i += 1
 
     with open(options.output, "w") as output_file:
